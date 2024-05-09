6b30a449569f5c92cd230862c0858f8f98d04987
Flaxelaxen <mrkaland95@gmail.com>
2024-05-09 21:14:50 +0200

    fix: next layer set parser now correctly matches the various combinations of the setnextlayer command, i.e layer, layer+factions, or layer+factions+subfactions

--- squad-server/log-parser/next-layer-set.js
+++ squad-server/log-parser/next-layer-set.js
@@ -1,6 +1,6 @@
 export default {
   // [2023.12.13-03.50.09:051][410]LogSquad: ADMIN COMMAND: Set next layer to AlBasrah_RAAS_v1 from 骀徥ȶ
-  regex: /^\[([0-9.\-:]+)]\[([0-9]+)]LogSquad: ADMIN COMMAND: Set next layer to (\S+)(?: ([^\[]+))? from .+/,
+  regex: /^\[([0-9\.\-:]+)]\[([0-9]+)]LogSquad: ADMIN COMMAND: Set next layer to ([^\s]+)(?: ([^\[]+))? from .+/,
   onMatch: (args, logParser) => {
     const data = {
       raw: args[0],
