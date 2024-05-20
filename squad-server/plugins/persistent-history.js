b6fc2fb518fd97c66f740bead963e1b00391abb0
Flaxelaxen <mrkaland95@gmail.com>
2024-05-20 20:29:08 +0200

    Adjusted !maps and !tickets commands, !maps will show matches along with their end time, !tickets will show matches with their factions, subfactions and ticket differential.

--- squad-server/plugins/persistent-history.js
+++ squad-server/plugins/persistent-history.js
@@ -50,7 +50,7 @@ export default class PersistentHistory extends BasePlugin {
 
   async mount() {
     this.DBLogPlugin = this.server.plugins.find(p => p instanceof DBLog);
-    if (!this.DBLogPlugin) return;
+    if (!this.DBLogPlugin) { this.verbose(1, 'Could not find db plugin'); return }
 
     await this.updateLayerHistory()
     this.verbose(1, 'Loaded layer history from database...')
